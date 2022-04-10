b04d03041778aae6f8eb9dad20b1d1caf4cf12c2
Migel Tom <kingtiger0723@gmail.com>
2022-04-10 16:45:10 +0900

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0x68532D246D0B56F5f9cb5bd18850880bCb0A87F6";
+export const contractAddress = process.env.CONTRACT_ADDRESS;
 export const contractABI = abi.abi;
